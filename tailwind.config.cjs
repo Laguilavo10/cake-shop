ba95fc501f9cd77ab92b991851517a4a719c6c69
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-07-27 15:35:01 -0500

    add hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				principal: '#fce9eb',
+				secondary: '#5D5D81'
+			}
+		},
 	},
 	plugins: [],
 }
