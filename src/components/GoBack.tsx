d88df5e067fa8941863120f68b3e8dfa91e5f469
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-11-28 15:45:15 -0500

    Fix layout and component styles

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -5,7 +5,7 @@ const GoBack = () => {
   };
 
   return (
-  <button onClick={handleGoBack} className='absolute z-20 ml-5 flex cursor-pointer items-center gap-2 pt-10 md:ml-20'>
+  <button onClick={handleGoBack} className='absolute z-20  flex cursor-pointer items-center -top-16'>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
     </svg>
