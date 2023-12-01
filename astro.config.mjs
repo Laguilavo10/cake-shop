07ae69b36dc7d981a85135a2018d0c9a2ff3a297
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-11-30 20:28:56 -0500

    Remove @astrojs/netlify from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
